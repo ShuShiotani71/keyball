fc81aab8f5aa99fd7d269c596b3ad50e84d43587
Shu Shiotani <shu.shiotani70@gmail.com>
2025-07-05 20:57:57 +0900

    add: combo for left super key

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/shiotani/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/shiotani/keymap.c
@@ -85,6 +85,7 @@ enum combos{
   LEFT_CTRL_SHIFT,
   RIGHT_CTRL_SHIFT,
   LEFT_ALT,
+  LEFT_GUI,
   BTN1,
   BTN2,
 };
@@ -98,6 +99,7 @@ const uint16_t PROGMEM left_ctrl_shift[] = {KC_S, KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM right_ctrl_shift[] = {KC_J, KC_K, KC_L, COMBO_END};
 
 const uint16_t PROGMEM left_alt[] = {KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM left_gui[] = {KC_X, KC_C, COMBO_END};
 
 const uint16_t PROGMEM btn1[] = {KC_M, KC_COMM, COMBO_END};
 const uint16_t PROGMEM btn2[] = {KC_COMM, KC_DOT, COMBO_END};
@@ -110,6 +112,7 @@ combo_t key_combos[] = {
   [LEFT_CTRL_SHIFT] = COMBO(left_ctrl_shift, KC_LEFT_CTRL_SHIFT),
   [RIGHT_CTRL_SHIFT] = COMBO(right_ctrl_shift, KC_RIGHT_CTRL_SHIFT),
   [LEFT_ALT] = COMBO(left_alt, KC_LALT),
+  [LEFT_GUI] = COMBO(left_gui, KC_LGUI),
   [BTN1] = COMBO(btn1, KC_BTN1),
   [BTN2] = COMBO(btn2, LT(3, KC_BTN2)),
 };
