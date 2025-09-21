6224843f6e404dfb543a4f539de7473a3eaf7915
Shu Shiotani <shu.shiotani70@gmail.com>
2025-09-21 16:52:23 +0900

    comment out: rgb

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/shiotani/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/shiotani/rules.mk
@@ -1,4 +1,4 @@
-//RGBLIGHT_ENABLE = yes
+# RGBLIGHT_ENABLE = yes
 
 OLED_ENABLE = yes
 
@@ -6,8 +6,12 @@ VIA_ENABLE = yes
 
 COMBO_ENABLE = yes
 
+# media buttons
 EXTRAKEY_ENABLE = yes
 
+# scrolling using buttons
 MOUSEKEY_ENABLE = yes
 
+# for sm tap dance
 DEFERRED_EXEC_ENABLE = yes
+
