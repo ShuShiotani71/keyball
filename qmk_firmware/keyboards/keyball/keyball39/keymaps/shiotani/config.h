c54944adbaa0ad755b5e6125d3ae4b96d4b3f1b9
Shu Shiotani <shu.shiotani70@gmail.com>
2025-10-18 10:18:16 +0900

    add: combo allow action key

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/shiotani/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/shiotani/config.h
@@ -42,4 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
 
 // for mod tap
-#define TAPPING_TERM 400
+#define TAPPING_TERM 300
+
+// allow combos to coexit with mod tap
+#define COMBO_ALLOW_ACTION_KEYS
