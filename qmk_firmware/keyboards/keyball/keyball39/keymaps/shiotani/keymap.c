46873f0e6fe18a4cb5ac5c0a617c591570b88275
Shu Shiotani <shu.shiotani70@gmail.com>
2025-11-02 09:42:43 +0900

    refine: keyamp

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/shiotani/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/shiotani/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q               , KC_W               , KC_E               , KC_R               , KC_T               ,                                           KC_Y               , KC_U               , KC_I               , KC_O               , KC_P               ,
     KC_A               , KC_S               , MT(MOD_LGUI, KC_D) , MT(MOD_LALT, KC_F) , KC_G               ,                                           KC_H               , MT(MOD_RALT, KC_J) , MT(MOD_RGUI, KC_K) , LT(3, KC_L)        , KC_BSPC            ,
     KC_Z               , KC_X               , KC_C               , KC_V               , KC_B               ,                                           KC_N               , KC_M               , KC_COMM            , KC_DOT             , KC_ENT             ,
-    _______            , _______            , _______            , _______            , MO(1)              , MT(MOD_LSFT, KC_ENT) , MT(MOD_RCTL, KC_SPC) , MO(2)          , _______            , _______            , _______            , _______
+    _______            , _______            , _______            , _______            , MO(1)              , KC_LSFT           , MT(MOD_RCTL, KC_SPC) , MO(2)          , _______            , _______            , _______            , _______
   ),
 
   [1] = LAYOUT_universal(
