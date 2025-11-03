01c5805620d931e7d239766ba3368a6f698b807d
Shu Shiotani <shu.shiotani70@gmail.com>
2025-11-03 16:27:07 +0900

    refine: keymap

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/shiotani/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/shiotani/keymap.c
@@ -21,47 +21,63 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "quantum.h"
 
 
-// come up with something better
-const key_override_t comm_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_TILD);
-const key_override_t dot_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_HASH);
+// const key_override_t comm_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_TILD);
+// const key_override_t dot_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_HASH);
 
-const key_override_t **key_overrides = (const key_override_t *[]){
-	&comm_key_override,
-	&dot_key_override,
-	NULL
-};
+// const key_override_t **key_overrides = (const key_override_t *[]){
+	// &comm_key_override,
+	// &dot_key_override,
+	// NULL
+// };
 
+#define D_LGUI MT(MOD_LGUI, KC_D)
+#define F_LALT MT(MOD_LALT, KC_F)
+#define K_RGUI MT(MOD_RGUI, KC_K)
+#define J_RALT MT(MOD_RALT, KC_J)
+#define L_LT3 LT(3, KC_L)
+
+#define SPC_RCTL MT(MOD_RCTL, KC_SPC)
+
+#define SCRN_SHT LSFT(LGUI(KC_S))
+#define WIN_SEC LCTL(LALT(KC_DEL))
+#define CH_LANG RCTL(KC_SPC)
+
+#define SCR_LEFT KC_MS_WH_LEFT
+#define SCR_DOWN KC_MS_WH_DOWN
+#define SCR_UP KC_MS_WH_UP
+#define SCR_RGHT KC_MS_WH_RIGHT
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [0] = LAYOUT_universal(
-    KC_Q               , KC_W               , KC_E               , KC_R               , KC_T               ,                                           KC_Y               , KC_U               , KC_I               , KC_O               , KC_P               ,
-    KC_A               , KC_S               , MT(MOD_LGUI, KC_D) , MT(MOD_LALT, KC_F) , KC_G               ,                                           KC_H               , MT(MOD_RALT, KC_J) , MT(MOD_RGUI, KC_K) , LT(3, KC_L)        , KC_BSPC            ,
-    KC_Z               , KC_X               , KC_C               , KC_V               , KC_B               ,                                           KC_N               , KC_M               , KC_COMM            , KC_DOT             , KC_ENT             ,
-    _______            , _______            , _______            , _______            , MO(1)              , KC_LSFT           , MT(MOD_RCTL, KC_SPC) , MO(2)          , _______            , _______            , _______            , _______
+      KC_Q         , KC_W         , KC_E         , KC_R         , KC_T         ,                               KC_Y         , KC_U         , KC_I         , KC_O         , KC_P         ,
+      KC_A         , KC_S         , D_LGUI       , F_LALT       , KC_G         ,                               KC_H         , J_RALT       , K_RGUI       , L_LT3        , KC_BSPC      ,
+      KC_Z         , KC_X         , KC_C         , KC_V         , KC_B         ,                               KC_N         , KC_M         , KC_COMM      , KC_DOT       , KC_ENT       ,
+      _______      , _______      , _______      , _______      , MO(1)        , KC_LSFT      , SPC_RCTL     , MO(2)        , _______      , _______      , _______      , _______
   ),
 
   [1] = LAYOUT_universal(
-    KC_GRV             , KC_PERC            , KC_CIRC            , KC_DLR             , KC_PIPE            ,                                           KC_AT              , KC_QUOT            , KC_LBRC            , KC_RBRC            , KC_SCLN            ,
-    KC_EXLM            , KC_QUES            , KC_MINS            , KC_UNDS            , _______            ,                                           _______            , KC_DQT             , KC_LPRN            , KC_RPRN            , KC_COLN            ,
-    KC_ASTR            , KC_LABK            , KC_RABK            , KC_AMPR            , KC_BSLS            ,                                           KC_PLUS            , KC_EQL             , KC_LCBR            , KC_RCBR            , KC_SLSH            ,
-    _______            , _______            , _______            , _______            , _______            , _______             , MT(MOD_RCTL, KC_SPC), _______            , _______            , _______            , _______            , _______
+      _______      , KC_PERC      , KC_CIRC      , KC_DLR       , KC_GRV       ,                               KC_AT        , KC_QUOT      , KC_LBRC      , KC_RBRC      , _______      ,
+      KC_EXLM      , KC_QUES      , KC_MINS      , KC_UNDS      , KC_PIPE      ,                               KC_SCLN      , KC_DQT       , KC_LPRN      , KC_RPRN      , KC_COLN      ,
+      KC_ASTR      , KC_TILD      , KC_HASH      , KC_AMPR      , KC_BSLS      ,                               KC_PLUS      , KC_EQL       , KC_LCBR      , KC_RCBR      , KC_SLSH      ,
+      _______      , _______      , _______      , _______      , _______      , _______      , SPC_RCTL     , _______      , _______      , _______      , _______      , _______
   ),
-  
+
   [2] = LAYOUT_universal(
-    _______            , KC_1               , KC_2               , KC_3               , _______            ,                                           _______            , KC_DEL             , KC_BSPC            , RCTL(KC_SPC)       , _______            ,
-    KC_0               , KC_4               , KC_5               , KC_6               , _______            ,                                           KC_LEFT            , KC_DOWN            , KC_UP              , KC_RGHT            , KC_ESC             ,
-    _______            , KC_7               , KC_8               , KC_9               , _______            ,                                           KC_MS_WH_LEFT      , KC_MS_WH_DOWN      , KC_MS_WH_UP        , KC_MS_WH_RIGHT     , KC_CAPS            ,
-    _______            , _______            , _______            , _______            , _______            , MT(MOD_LSFT, KC_ENT), _______           , _______            , _______            , _______            , _______            , _______
+      _______      , KC_1         , KC_2         , KC_3         , _______      ,                               _______      , KC_CAPS      , KC_DEL       , CH_LANG      , _______      ,
+      KC_0         , KC_4         , KC_5         , KC_6         , _______      ,                               KC_LEFT      , KC_DOWN      , KC_UP        , KC_RGHT      , KC_ESC       ,
+      _______      , KC_7         , KC_8         , KC_9         , _______      ,                               SCR_LEFT     , SCR_DOWN     , SCR_UP       , SCR_RGHT     , _______      ,
+      _______      , _______      , _______      , _______      , _______      , KC_LSFT      , _______      , _______      , _______      , _______      , _______      , _______
   ),
-  
+
   [3] = LAYOUT_universal(
-    _______            , _______            , _______            , _______            , _______            ,                                           _______            , _______            , _______            , _______            , _______            ,
-    KC_MUTE            , KC_VOLD            , KC_VOLU            , LSFT(LGUI(KC_S))   , _______            ,                                           _______            , KC_MS_BTN4         , KC_MS_BTN5         , _______            , _______            ,
-    _______            , KC_BRID            , KC_BRIU            , LCTL(LALT(KC_DEL)) , _______            ,                                           _______            , _______            , _______            , _______            , _______            ,
-    _______            , _______            , _______            , _______            , _______            , MT(MOD_LSFT, KC_ENT),MT(MOD_RCTL, KC_SPC), _______            , _______            , _______            , _______            , _______
+      _______      , _______      , _______      , _______      , _______      ,                               _______      , _______      , _______      , _______      , _______      ,
+      KC_MUTE      , KC_VOLD      , KC_VOLU      , SCRN_SHT     , _______      ,                               _______      , KC_MS_BTN4   , KC_MS_BTN5   , _______      , _______      ,
+      _______      , KC_BRID      , KC_BRIU      , WIN_SEC      , _______      ,                               _______      , _______      , _______      , _______      , _______      ,
+      _______      , _______      , _______      , _______      , _______      , KC_LSFT      , SPC_RCTL     , _______      , _______      , _______      , _______      , _______
   ),
+
 
 };
 // clang-format on
@@ -108,10 +124,8 @@ combo_t key_combos[] = {
 // per key tapping term
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case MT(MOD_LSFT, KC_ENT):
-            return g_tapping_term - 50; // g_tapping_term used instead of TAPPING_TERM since dynamic_tapping_term enabled
         case MT(MOD_RCTL, KC_SPC):
-            return g_tapping_term - 50;
+            return g_tapping_term - 50; // g_tapping_term used instead of TAPPING_TERM since dynamic_tapping_term enabled
         default:
             return g_tapping_term;
     }
