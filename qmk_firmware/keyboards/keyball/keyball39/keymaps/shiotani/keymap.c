fe3dd31757ca1060ffb17e14018da6c92809a7e5
Shu Shiotani <shu.shiotani70@gmail.com>
2025-11-06 23:47:04 +0900

    delete: unecessry bits

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/shiotani/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/shiotani/keymap.c
@@ -30,22 +30,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	// NULL
 // };
 
-enum custom_keycodes {
-    OKAY = SAFE_RANGE,
-};
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-    case OKAY:
-        if (record->event.pressed) {
-            SEND_STRING("ｵｯｹｰ☆⌒d(´∀｀)ノ");
-        } else {
-        }
-        break;
-    }
-    return true;
-};
-
 
 #define D_LGUI MT(MOD_LGUI, KC_D)
 #define F_LALT MT(MOD_LALT, KC_F)
