593c9cf20332f60ad265f82cc1dc80fe11e97dad
ShuShiotani71 <shu.shiotani70@gmail.com>
2026-01-09 16:43:01 +0000

    refine: keymap

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/shiotani/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/shiotani/keymap.c
@@ -212,6 +212,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #define K_RGUI MT(MOD_RGUI, KC_K)
 #define J_RALT MT(MOD_RALT, KC_J)
 #define L_LT3 LT(3, KC_L)
+#define S_LT4 LT(4, KC_S)
 
 #define SPC_RCTL MT(MOD_RCTL, KC_SPC)
 
@@ -229,8 +230,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [0] = LAYOUT_universal(
       KC_Q         , KC_W         , KC_E         , KC_R         , KC_T         ,                               KC_Y         , KC_U         , KC_I         , KC_O         , KC_P         ,
-      KC_A         , KC_S         , D_LGUI       , F_LALT       , KC_G         ,                               KC_H         , J_RALT       , K_RGUI       , L_LT3        , KC_BSPC      ,
-      KC_Z         , KC_X         , KC_C         , KC_V         , KC_B         ,                               KC_N         , KC_M         , KC_COMM      , KC_DOT       , KC_ESC       ,
+      KC_A         , S_LT4        , D_LGUI       , F_LALT       , KC_G         ,                               KC_H         , J_RALT       , K_RGUI       , L_LT3        , KC_BSPC      ,
+      KC_Z         , KC_X         , KC_C         , KC_V         , KC_B         ,                               KC_N         , KC_M         , KC_COMM      , KC_DOT       , _______      ,
       _______      , _______      , _______      , _______      , MO(1)        , MO(2)        , SPC_RCTL     , KC_RSFT      , _______      , _______      , _______      , _______
   ),
 
@@ -249,10 +250,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [3] = LAYOUT_universal(
-      _______      , _______      , _______      , THUMBSUP     , EMAIL        ,                               _______      , _______      , _______      , _______      , _______      ,
-      KC_MUTE      , KC_VOLD      , KC_VOLU      , SCRN_SHT     , NAME         ,                               _______      , SCR_DOWN     , SCR_UP       , _______      , _______      ,
-      _______      , KC_BRID      , KC_BRIU      , WIN_SEC      , BUNNY        ,                               _______      , KC_BTN4      , KC_BTN5      , _______      , _______      ,
-      _______      , _______      , _______      , _______      , MO(1)      , MO(2)          , SPC_RCTL     , KC_RSFT      , _______      , _______      , _______      , _______
+      _______      , _______      , _______      , _______      , _______      ,                               _______      , _______      , _______      , _______      , _______      ,
+      KC_MUTE      , KC_VOLD      , KC_VOLU      , SCRN_SHT     , _______      ,                               _______      , KC_BTN4      , KC_BTN5      , _______      , _______      ,
+      _______      , KC_BRID      , KC_BRIU      , WIN_SEC      , _______      ,                               _______      , _______      , _______      , _______      , _______      ,
+      _______      , _______      , _______      , _______      , MO(1)        , MO(2)        , SPC_RCTL     , KC_RSFT      , _______      , _______      , _______      , _______
+  ),
+
+  [4] = LAYOUT_universal(
+      _______      , _______      , _______      , _______      , _______      ,                               _______      , BUNNY        , THUMBSUP     , _______      , _______      ,
+      _______      , _______      , _______      , _______      , _______      ,                               _______      , EMAIL        , NAME         , _______      , _______      ,
+      _______      , _______      , _______      , _______      , _______      ,                               _______      , _______      , _______      , _______      , _______      ,
+      _______      , _______      , _______      , _______      , MO(1)        , MO(2)        , SPC_RCTL     , KC_RSFT      , _______      , _______      , _______      , _______
   ),
 
 
@@ -279,14 +287,12 @@ enum combos{
   BTN2,
   TAB,
   ENTER,
-  CHANGE_LANG,
   DELETE,
 };
 const uint16_t PROGMEM btn1[] = {MT(MOD_RGUI, KC_K), LT(3, KC_L), COMBO_END};
 const uint16_t PROGMEM btn2[] = {KC_COMM, KC_DOT, COMBO_END};
 const uint16_t PROGMEM tab[] = {KC_W, KC_E, COMBO_END};
 const uint16_t PROGMEM enter[] = {KC_I, KC_O, COMBO_END};
-const uint16_t PROGMEM change_lang[] = {KC_S, D_LGUI, COMBO_END};
 const uint16_t PROGMEM delete[] = {KC_X, KC_C, COMBO_END};
 
 combo_t key_combos[] = {
@@ -294,7 +300,6 @@ combo_t key_combos[] = {
   [BTN2] = COMBO(btn2, KC_BTN2),
   [TAB] = COMBO(tab, KC_TAB),
   [ENTER] = COMBO(enter, KC_ENT),
-  [CHANGE_LANG] = COMBO(change_lang, CH_LANG),
   [DELETE] = COMBO(delete, KC_DEL),
 };
 #endif
