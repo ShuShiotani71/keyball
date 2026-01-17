d4d21e22ac880555a1f23ea3576577b256f09c93
ShuShiotani71 <shu.shiotani70@gmail.com>
2026-01-17 23:51:22 +0900

    change: keymap

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/shiotani/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/shiotani/keymap.c
@@ -25,13 +25,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const key_override_t comm_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_BSLS);
 const key_override_t dot_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_GRV);
 const key_override_t bspc_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_ESC);
-const key_override_t pipe_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_PIPE, KC_AT);
 
 const key_override_t **key_overrides = (const key_override_t *[]){
     &comm_key_override,
     &dot_key_override,
     &bspc_key_override,
-    &pipe_key_override,
     NULL
 };
 
@@ -233,21 +231,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
       KC_Q         , KC_W         , KC_E         , KC_R         , KC_T         ,                               KC_Y         , KC_U         , KC_I         , KC_O         , KC_P         ,
       KC_A         , S_LT4        , D_LGUI       , F_LALT       , KC_G         ,                               KC_H         , J_RALT       , K_RGUI       , L_LT3        , KC_BSPC      ,
-      KC_Z         , KC_X         , KC_C         , KC_V         , KC_B         ,                               KC_N         , KC_M         , KC_COMM      , KC_DOT       , KC_PIPE      ,
+      KC_Z         , KC_X         , KC_C         , KC_V         , KC_B         ,                               KC_N         , KC_M         , KC_COMM      , KC_DOT       , TOGGLE_EN    ,
       _______      , _______      , _______      , _______      , MO(1)        , MO(2)        , SPC_RCTL     , KC_RSFT      , _______      , _______      , _______      , _______
   ),
 
   [1] = LAYOUT_universal(
-      KC_CIRC      , KC_LBRC      , KC_RBRC      , KC_DLR       , KC_PERC      ,                               ALT_SFT_TAB  , ALT_TAB      , CTL_SFT_TAB  , CTL_TAB      , KC_SCLN      ,
+      KC_CIRC      , KC_LBRC      , KC_RBRC      , KC_DLR       , KC_PERC      ,                               KC_AT        , KC_PIPE      , KC_TAB       , KC_ENT       , KC_SCLN      ,
       KC_MINS      , KC_LPRN      , KC_RPRN      , KC_UNDS      , KC_ASTR      ,                               KC_HASH      , KC_DQT       , KC_QUOT      , KC_EQL       , KC_COLN      ,
       KC_LABK      , KC_LCBR      , KC_RCBR      , KC_RABK      , KC_AMPR      ,                               KC_QUES      , KC_EXLM      , KC_PLUS      , KC_SLSH      , KC_TILD      ,
       _______      , _______      , _______      , _______      , _______      , _______      , SPC_RCTL     , KC_RSFT      , _______      , _______      , _______      , _______
   ),
 
   [2] = LAYOUT_universal(
-      _______      , KC_1         , KC_2         , KC_3         , _______      ,                               _______      , CH_LANG      , TOGGLE_JP    , TOGGLE_EN    , _______      ,
+      _______      , KC_1         , KC_2         , KC_3         , _______      ,                               ALT_SFT_TAB  , ALT_TAB      , CTL_SFT_TAB  , CTL_TAB      , _______      ,
       KC_0         , KC_4         , KC_5         , KC_6         , _______      ,                               KC_LEFT      , KC_DOWN      , KC_UP        , KC_RIGHT     , KC_BSPC      ,
-      _______      , KC_7         , KC_8         , KC_9         , _______      ,                               SCR_LEFT     , SCR_DOWN     , SCR_UP       , SCR_RGHT     , _______      ,
+      _______      , KC_7         , KC_8         , KC_9         , _______      ,                               SCR_LEFT     , SCR_DOWN     , SCR_UP       , SCR_RGHT     , TOGGLE_JP    ,
       _______      , _______      , _______      , _______      , _______      , _______      , SPC_RCTL     , KC_RSFT      , _______      , _______      , _______      , _______
   ),
 
